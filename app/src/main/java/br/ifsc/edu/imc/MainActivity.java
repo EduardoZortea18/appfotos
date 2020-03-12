b408fb3d18d117259174cd0f791ee15cb6b62383
Matheus Cipriani <matheuscipriani2015@outlook.com>
2020-03-12 11:14:53 -0300

    Commit aula Dia12 de março

--- app/src/main/java/br/ifsc/edu/imc/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/imc/MainActivity.java
@@ -6,17 +6,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
+    List<Integer> lista_imagens;
+    int posicaoImagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView);
+        lista_imagens = Arrays.asList(
+                R.drawable.cachorro,
+                R.drawable.patinho,
+                R.drawable.porquinho,
+                R.drawable.gardem,
+                R.drawable.happy);
+
+        posicaoImagem = 0;
+
+
     }
-    public void mudatexto (View view) {
-        imageView.setImageResource(R.drawable.cachorro);
+
+
+
+    public void mudaFoto(View view){
+
+
+        for (int i=0; i < lista_imagens.size(); i++){
+            if (   posicaoImagem < lista_imagens.size()-1){
+                imageView.setImageResource(lista_imagens.get(posicaoImagem));
+            }
+            else{
+                posicaoImagem=0;
+                imageView.setImageResource(lista_imagens.get(posicaoImagem));
+            }
+            posicaoImagem++;
+        }
     }
 
 
